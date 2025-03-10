91db264bab3b5cf1663b9a01d6757595d308147f
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2025-03-10 15:51:28 +0100

    Remove rush's leftover instruction

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
       host-gpg-agent = ./nixos/services/host-gpg-agent.nix;
       nebula = ./nixos/services/nebula.nix;
       remote-builder = ./nixos/nix/remote-builder.nix;
-      rush = ./nixos/programs/rush.nix;
     };
   };
 }
